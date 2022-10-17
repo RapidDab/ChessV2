a866a693f9e4a121d227ad2121d392be7fa55e56
Jonathan <soojonathan07@gmail.com>
2022-10-17 10:24:01 -0700

    Working on legal moves for the King

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import os
 
-import pygame
 import pygame as py
 import json
-print("Hello world")
+#Hello world
 py.init()
 class Pieces:
     def __init__(self):
@@ -16,7 +15,7 @@ class Pieces:
         self.rect = []
         self.p = "rnbqkbnrp"
         # nbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNRr
-        self.fenString = "nbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNRr"
+        self.fenString = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNRr"
 
     def loadImages(self):
         os.chdir("C:\ChessV2\Player1")
@@ -26,7 +25,7 @@ class Pieces:
         for i in os.listdir("C:\ChessV2\Player2"):
             self.player2.append(py.transform.scale(py.image.load(i).convert_alpha(), self.imageSize))
 
-    def getImageFile(self, char: str) -> pygame.Rect:
+    def getImageFile(self, char: str) -> py.Rect:
         file = ""
         if char.islower():
             os.chdir("C:\ChessV2\Player1")
@@ -47,17 +46,34 @@ class Pieces:
                 x = 0
             else:
                 self.gameRect.append(s)
-                self.gameCor.append([x, y])
+                self.gameCor.append([i, x, y])
                 x += 100
 
     def drawPieces(self):
         for i, num in enumerate(self.gameCor):
-            screen.blit(self.gameRect[i], (num[0], num[1]))
+            screen.blit(self.gameRect[i], (num[1], num[2]))
 
     def makeRect(self):
         for i in self.gameCor:
-            self.rect.append(pygame.Rect((i[0], i[1]), (98, 98)))
+            self.rect.append(py.Rect((i[1], i[2]), (98, 98)))
     # def getCor(self, index):
+class GameLogic(Pieces):
+    def __init__(self):
+        super().__init__()
+        self.r = []
+        self.n = []
+        self.b = []
+        self.k = []
+        self.q = []
+        self.p = []
+        self.moves = [[0, 100], [0, -100], [100, 100], [-100, 100], [100, -100], [-100, -100]]
+        self.piecesToMove = ""
+        
+    def legalMoves(self):
+        cor = [self.gameCor[movePiece][1], self.gameCor[movePiece][2]]
+        if self.piecesToMove.islower():
+            if self.piecesToMove == "k":
+                print(cor)
 
 
 # Loading screen variable and the bacground image
@@ -66,16 +82,13 @@ bg = py.image.load("unnamed.jpg").convert()
 bg = py.transform.scale(bg, (800, 800))
 
 # Pieces methods needed to initialize
-pieces = Pieces()
-pieces.loadImages()
-pieces.fenParse()
-pieces.makeRect()
-
+chess = GameLogic()
+chess.loadImages()
+chess.fenParse()
+chess.makeRect()
 # Game loop variables
 loop = True
 clicked = False
-movePiece = ""
-
 while loop:
     for event in py.event.get():
         if event.type == py.QUIT:
@@ -83,25 +96,30 @@ while loop:
         if event.type == py.MOUSEBUTTONDOWN:
             if event.button == 1:
                 x, y = event.pos
-                for i in pieces.rect:
+                for i in chess.rect:
                     if i.collidepoint(event.pos):
-                        movePiece = pieces.rect.index(i)
+                        movePiece = chess.rect.index(i)
                         startX, starty = event.pos
-                        print(i)
                         clicked = True
+                        chess.piecesToMove = chess.gameCor[movePiece][0]
+                        chess.legalMoves()
+                        
         elif event.type == py.MOUSEMOTION:
             if clicked:
                 mouse_x, mouse_y = event.pos
-                pieces.gameCor[movePiece][0] = mouse_x - pieces.imageSize[0] / 2
-                pieces.gameCor[movePiece][1] = mouse_y - pieces.imageSize[1] / 2
+                chess.gameCor[movePiece][1] = mouse_x - chess.imageSize[0] / 2
+                chess.gameCor[movePiece][2] = mouse_y - chess.imageSize[1] / 2
         elif event.type == py.MOUSEBUTTONUP:
             if event.button == 1:
                 clicked = False
                 end_x, end_y = event.pos
-                pieces.gameCor[movePiece][0] = round(end_x - 50, -2)
-                pieces.gameCor[movePiece][1] = round(end_y - 50, -2)
+                try:
+                    chess.gameCor[movePiece][1] = round(end_x - 50, -2)
+                    chess.gameCor[movePiece][2] = round(end_y - 50, -2)
+                    movePiece = None
+                except:
+                    pass
     screen.blit(bg, (0, 0))
-    pieces.drawPieces()
+    chess.drawPieces()
     py.display.flip()
 py.quit()
-#hello world
